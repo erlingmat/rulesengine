0788975201023634bd76d8a6ee15adea81b60d13
Erling Møller <emm@connectedcars.dk>
2021-01-27 18:52:21 +0100

    Tests for special video title

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
+import com.mycompany.product.*;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -12,6 +14,32 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    @Test
+    public void PhysicalProductShouldGeneratePacking() {
+	Book thisBook = new Book();
+	String actionList = thisBook.orderAction();
+	assertEquals(actionList, "PackingslipAction, RoyaltyOrderAction");
+	
+    }
+    @Test
+    public void VideoProductShouldGeneratePacking () {
+	Video thisVideo = new Video();
+	String actionList = thisVideo.orderAction();
+	assertEquals(actionList, "PackingslipAction");
+    }
+    @Test
+    public void VideoLearningToSkiShouldGenerateAdditionalPacking() {
+	Video thisVideo = new Video();
+	thisVideo.title = "Learning to Ski";
+	String actionList = thisVideo.orderAction();
+	assertEquals(actionList, "PackingslipAction, PackingFirstAid");
+    }
+    @Test
+    public void MembershipShouldActivateAndSendEmail() {
+	Membership thisMembership = new Membership();
+	String actionList = thisMembership.orderAction();
+	assertEquals(actionList, "MembershipActivationAction, MembershipActivationEmail");
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
